8c06226efd56f368b034eafa65a798ef273001fe
Nicholas Zehr <nicholas.zehr@icloud.com>
2021-09-22 15:13:04 -0500

    Made cart-checkout folder and cart component

---
+++ client/components/cart-checkout/Cart.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { productActions } from '../../store/ActionsCreators';
+
+const Cart = () => {
+  const { cart } = useSelector((state) => state.cart);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function fetchData() {
+      await dispatch(cartActions.fetchCart());
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className='Cart'>
+      {cart.items.length > 0 ? (
+        cart.map((eachProduct) => (
+          <ProductCell key={eachProduct.id} item={eachProduct} />
+        ))
+      ) : (
+        <p>Loading</p>
+      )}
+    </div>
+  );
+};
+
+export default Products;
