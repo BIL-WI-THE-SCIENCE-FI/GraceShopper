13694593e4d4e1e165abfe3e454c9a89f3eec167
TehBunk <tehbunk@gmail.com>
2021-09-22 10:31:00 -0500

    fixed express route for getting products

--- client/Routes.js
+++ client/Routes.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm'
+import Products from './components/products/Products'
 import Home from './components/Home'
 import { me } from './store'
 /**
@@ -27,7 +28,7 @@ class Routes extends Component {
             <Route path="/" exact component={Login} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
-            <Route path="/products" component={Products}/>
+            <Route path="/products" component={Products} />
           </Switch>
         )}
       </div>
