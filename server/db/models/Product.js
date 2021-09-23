0a328650c34b2f296261c9a1238148a501b41644
Nicholas Zehr <nicholas.zehr@icloud.com>
2021-09-23 12:13:17 -0500

    simple changes

--- server/db/models/Product.js
+++ server/db/models/Product.js
@@ -1,6 +1,5 @@
-const Sequelize = require('sequelize')
-const { format } = require('sequelize/types/lib/utils')
-const db = require('../db')
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 /*
  * This is the model for each Product.
@@ -19,20 +18,20 @@ const Product = db.define('product', {
   //* Product Name is a string that cannot be null
   name: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
   //* Descrption is a large text that cannot be null
   description: {
     type: Sequelize.TEXT,
-    allowNull: false
+    allowNull: false,
   },
   //* Price is the amount of pennies that the product costs
   price: {
     type: Sequelize.INTEGER,
     allowNull: false,
     validate: {
-      min: 0
-    }
+      min: 0,
+    },
   },
   //* Stock is the current number of products in inventory
   stock: {
@@ -40,8 +39,8 @@ const Product = db.define('product', {
     allowNull: false,
     defaultValue: 0,
     validate: {
-      min: 0
-    }
+      min: 0,
+    },
   },
   //* Rating is a double that can be between 0 and 5
   rating: {
@@ -49,14 +48,14 @@ const Product = db.define('product', {
     allowNull: false,
     validate: {
       min: 0.0,
-      max: 5.0
-    }
+      max: 5.0,
+    },
   },
   //* ImageUrl is the url to the image, cannot be null
   imageUrl: {
     type: Sequelize.STRING,
-    allowNull: false
-  }
-})
+    allowNull: false,
+  },
+});
 
-module.exports = Product
+module.exports = Product;
