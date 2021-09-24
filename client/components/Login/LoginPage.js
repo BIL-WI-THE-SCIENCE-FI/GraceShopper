6b9c39526b87816c786b44abdd0251952d92479d
Jing Lin <linjinglinjing123@gmail.com>
2021-09-24 18:38:33 -0400

    sign in sign out bugs fixed

--- client/components/Login/LoginPage.js
+++ client/components/Login/LoginPage.js
@@ -1,59 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { authenticate } from '../../store';
 // import 'simplebar/dist/simplebar.min.css'
 
 const LoginPage = () => {
   const dispatch = useDispatch();
-  // const { login, setLogin } = useState('login')
-  // const {signup, setSignup} = useState('signup')
-  //const {auth} = useSelector(state=>state.auth)
+  const isLoggedIn = useSelector((state) => state.auth.id);
   const handleSubmit = (evt) => {
     evt.preventDefault();
     const formName = evt.target.name;
     const username = evt.target.username.value;
     const password = evt.target.password.value;
-    console.log(formName, username, password)
     dispatch(authenticate(username, password, formName));
   };
   return (
     <div className='login'>
-      <div className='loginbody'>
-        <div>
-          <h1>Sign into your account</h1>
-        </div>
-        <form className='form' onSubmit={handleSubmit} name='login'>
-          <div className='emailBox'>
-            <input
-              className='email'
-              name='username'
-              type='text'
-              placeholder='Email or username'
-            />
+      {isLoggedIn ? (
+        <Redirect to='/home' />
+      ) : (
+        <div className='loginbody'>
+          <div>
+            <h1>Sign into your account</h1>
           </div>
-          <div className='emailBox'>
-            <input
-              className='email'
-              name='password'
-              placeholder='Password'
-              type='password'
-            />
-          </div>
+          <form className='form' onSubmit={handleSubmit} name='login'>
+            <div className='emailBox'>
+              <input
+                className='email'
+                name='username'
+                type='text'
+                placeholder='Email or username'
+              />
+            </div>
+            <div className='emailBox'>
+              <input
+                className='email'
+                name='password'
+                placeholder='Password'
+                type='password'
+              />
+            </div>
 
-          <div id='keepSignIn'>
-            <input type='checkbox' id='box' name='keep' value='yes' />
-            <label htmlFor='keep'> Keep me signed in</label>
-          </div>
-          <button className='signin' name='button1'>
-            sign in
-          </button>
-        </form>
-        <Link to='/signup'>
-          <button className='signup'>Create your account</button>
-        </Link>
-      </div>
+            <button className='signin' name='button1'>
+              sign in
+            </button>
+          </form>
+          <Link to='/signup'>
+            <button className='signup'>Create your account</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
