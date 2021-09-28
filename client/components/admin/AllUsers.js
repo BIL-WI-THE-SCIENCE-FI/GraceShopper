b50902b67b51bae28314721c6fa48d1d938273e9
Nicholas Zehr <nicholas.zehr@icloud.com>
2021-09-28 11:05:37 -0500

    Added AllUsers and UserCard call

--- client/components/admin/AllUsers.js
+++ client/components/admin/AllUsers.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from '../../store/ActionsCreators/userActions';
+import UserCard from '../admin/UserCard';
 
 //* This is the component that should render when an admin types /users
 //* It will allow them to view all individual users
 export default function AllUsers(props) {
-  const isLoggedIn = useSelector(state => state.auth.id)
-  const dispatch = useDispatch(function)
-  const [users, setUsers] = useState()
-  const usersInView = us
+  const isLoggedIn = useSelector((state) => state.auth.id);
+  const dispatch = useDispatch();
+  const [users, setUsers] = useState(dispatch(fetchUsers));
+  const usersInView = users;
 
   return (
-    <div className="product-page">
-      <div className="product-container">
-        <SimpleBar className="product-scroll">
-          <div className="products-container">
-            {productsInView !== undefined && productsInView.length > 0 ? (
-              productsInView.map(product => <ProductCard key={product.id} product={product} />)
+    <div className='product-page'>
+      <div className='product-container'>
+        <SimpleBar className='product-scroll'>
+          <div className='products-container'>
+            {usersInView !== undefined && usersInView.length > 0 ? (
+              usersInView.map((product) => (
+                <UserCard key={user.id} user={user} />
+              ))
             ) : (
               <p>Loading...</p>
             )}
@@ -22,5 +27,5 @@ export default function AllUsers(props) {
         </SimpleBar>
       </div>
     </div>
-  )
+  );
 }
--- client/components/admin/UserCard.js
+++ client/components/admin/UserCard.js
@@ -1,29 +1,40 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { useHistory } from 'react-router'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 
 //* This is the product card that will appear on the all products
 //* page.
-export default function ProductCard(props) {
-  const history = useHistory()
+export default function UserCard(props) {
+  const history = useHistory();
   //* Obtain the information required thru props
-  const { id, username, userType, firstName, lastName, phone, imageUrl, email } = props.user
-  const isLoggedIn = useSelector(state => state.auth.id)
-  const userType = useSelector(state => state.auth.userType)
+  const {
+    id,
+    username,
+    userType,
+    firstName,
+    lastName,
+    phone,
+    imageUrl,
+    email,
+  } = props.user;
+  const isLoggedIn = useSelector((state) => state.auth.id);
+  const userType = useSelector((state) => state.auth.userType);
   //* Return the component JSX
   return (
-    <div className="product-card shadow zoomable-small">
+    <div className='product-card shadow zoomable-small'>
       <div>
         <div onClick={() => history.push(`/products/${id}`)}>
           <div>
-            <img src={imageUrl} alt="Product Image" />
-            <h4>{firstName}, {lastName}</h4>
+            <img src={imageUrl} alt='Product Image' />
+            <h4>
+              {firstName}, {lastName}
+            </h4>
           </div>
-          <div className="content">
-            <div className="desc">
+          <div className='content'>
+            <div className='desc'>
               <span>{user}</span>
             </div>
-            <div className="info">
+            <div className='info'>
               <p>{`Phone: ${phone}`}</p>
               <p>{`Email: ${email}`}</p>
             </div>
@@ -31,9 +42,12 @@ export default function ProductCard(props) {
         </div>
         {isLoggedIn && userType === 'admin' ? (
           <div>
-            <button onClick={() => history.push(`edit/user/${id}`)}>Edit</button>
+            <button onClick={() => history.push(`edit/user/${id}`)}>
+              Edit
+            </button>
           </div>
         ) : null}
       </div>
     </div>
-  )
+  );
+}
